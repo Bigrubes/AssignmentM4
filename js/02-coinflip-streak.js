64e5b5427cf60d9204cbd4fd30e569c1962922c2
Reuben Ramos <rsramos23@gmail.com>
2023-10-10 23:58:21 -0700

    triangle complete

--- js/02-coinflip-streak.js
+++ js/02-coinflip-streak.js
@@ -1,1 +1,13 @@
-let coinFlip
+let coinFlip = 0
+do{
+    let num = Math.round(Math.random())
+    console.log(coinFlip)
+    if(num === 0){
+    console.log("Heads")
+    }else{
+        console.log("Tails")
+    }
+} while (coinFlip === "Tails")
+console.log( "Tails, End of game")
+
+
---
+++ js/03-triangle.js
@@ -0,0 +1,25 @@
+
+for (i=1;i<8;i++){
+    if(i === 1){
+    console.log("#")
+    }
+    else if(i === 2){
+    console.log("##")
+    }
+    else if(i === 3){
+    console.log("###")
+    }
+    else if(i === 4){
+    console.log("####")
+    }
+    else if(i === 5){
+    console.log("#####")
+    }
+    else if(i === 6){
+    console.log("######")
+    }
+    else{ (i === 7)
+    console.log("#######")
+    }
+// console.log(i)
+}
