d573d1c89319cb048913e32666b5aa32d11e068b
Reuben Ramos <rsramos23@gmail.com>
2023-10-10 22:52:36 -0700

    coinflip redux code cleaned up

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -1,8 +1,6 @@
 
 let num = prompt("How many times do you want to loop?")
 console.log(num)
-// let coinFlip = Math.round(Math.random())
-// console.log(coinFlip)
     for (let i = 0; i<num; i++){
         let coinFlip = Math.round(Math.random())
         console.log(coinFlip)
