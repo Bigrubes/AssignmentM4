b5e5db062eed0495c04628cdde39fb0134b0774b
Reuben Ramos <rsramos23@gmail.com>
2023-10-11 00:50:06 -0700

    marco polo completed

---
+++ js/05-marco-polo.js
@@ -0,0 +1,13 @@
+for(i=1;i<101;i++){
+if (i%3===0){
+    // console.log(i)
+    console.log("Marco!")
+}if(i%5===0){
+    // console.log(i)
+    console.log("Polo!")
+}if
+    (i%3===0 && i%5===0){
+    // console.log(i)
+    console.log("Marco Polo!")
+}
+}
