ea6800f7982534e9496ec9761071a2d89990bbbb
Reuben Ramos <rsramos23@gmail.com>
2023-10-10 22:36:33 -0700

    started coinflip redux

---
+++ js/01-coinflip-redux.js
@@ -0,0 +1,18 @@
+function coin(){
+ let x = 5
+ console.log(x)   
+let num = prompt("How many times do you want to loop?")
+console.log(num)
+let coinFlip = Math.round(Math.random())
+console.log(coinFlip)
+// let num = prompt("how many times do you want to loop?")
+    // for (let i = 0; i<num; i++){
+    //     if (coinFlip = 0){
+    //     console.log("Heads")
+    //     }else{
+    //         console.log("Tails")
+    //     }
+
+    // }
+}
+
