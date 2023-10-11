63a2e17594f8d9c21b1518bca1d3739bcaf6982f
Reuben Ramos <rsramos23@gmail.com>
2023-10-11 12:35:55 -0700

    comment out console.log in coinflip redux

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -1,9 +1,9 @@
 
 let num = prompt("How many times do you want to loop?")
-console.log(num)
+// console.log(num)
     for (let i = 0; i<num; i++){
         let coinFlip = Math.round(Math.random())
-        console.log(coinFlip)
+        // console.log(coinFlip)
         if (coinFlip === 0){
         console.log("Heads")
         }else{
